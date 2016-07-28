c1c06899cf0132b1ac6e7c68b65209cf47dbb7b7
Martin Kunz <martin.michael.kunz@gmail.com>
2016-07-28 13:57:21 +0200

    indent generated c code fix maven build

--- src/main/java/org/omilab/omirob/codegen/DispatchGen.java
+++ src/main/java/org/omilab/omirob/codegen/DispatchGen.java
@@ -23,16 +23,16 @@ public class DispatchGen {
 
         sb.append("class Dispatch {\n");
         sb.append(String.format("%s rob;\n",clazz.getSimpleName()));
-        sb.append("public: \n");
-        sb.append("void dispatch() {\n");
-        sb.append("byte fn=Serial.read();\n");
-        sb.append("switch(fn) {\n");
+        sb.append("\tpublic: \n");
+        sb.append("\tvoid dispatch() {\n");
+        sb.append("\t\tbyte fn=Serial.read();\n");
+        sb.append("\t\tswitch(fn) {\n");
         for(Method m:clazz.getMethods()) {
             TinyRPCMethod annotation = m.getDeclaredAnnotation(TinyRPCMethod.class);
-            sb.append(String.format("case %d: {\n",annotation.id()));
+            sb.append(String.format("\t\tcase %d: {\n",annotation.id()));
             sb.append("\n");
             sb.append(generateMethodCall(m));
-            sb.append("break; }\n");
+            sb.append("\t\tbreak; }\n");
         }
         sb.append("\n");
         sb.append("}\n");
@@ -49,25 +49,25 @@ public class DispatchGen {
             if(p.getType().equals(Short.TYPE)) {
                 String typeName=Tools.typeToC(Short.TYPE);
                 String varName="p"+i;
-                sb.append(String.format("%s %s;\n",typeName, varName));
-                sb.append(String.format("%s=Serial.read();\n"));
-                sb.append(String.format("%s<<8;\n"));
-                sb.append(String.format("%s|=Serial.read();\n"));
+                sb.append(String.format("\t\t\t%s %s;\n",typeName, varName));
+                sb.append(String.format("\t\t\t%s=Serial.read();\n"));
+                sb.append(String.format("\t\t\t%s<<8;\n"));
+                sb.append(String.format("\t\t\t%s|=Serial.read();\n"));
             }
             else if(p.getType().equals(Integer.TYPE)) {
                 String typeName=Tools.typeToC(Integer.TYPE);
                 String varName="p"+i;
-                sb.append(String.format("%s %s;\n",typeName, varName));
-                sb.append(String.format("%s=Serial.read();\n", varName));
-                sb.append(String.format("%s<<8;\n", varName));
-                sb.append(String.format("%s|=Serial.read();\n", varName));
-                sb.append(String.format("%s<<8;\n", varName));
-                sb.append(String.format("%s|=Serial.read();\n", varName));
-                sb.append(String.format("%s<<8;\n", varName));
-                sb.append(String.format("%s|=Serial.read();\n", varName));
+                sb.append(String.format("\t\t\t%s %s;\n",typeName, varName));
+                sb.append(String.format("\t\t\t%s=Serial.read();\n", varName));
+                sb.append(String.format("\t\t\t%s<<8;\n", varName));
+                sb.append(String.format("\t\t\t%s|=Serial.read();\n", varName));
+                sb.append(String.format("\t\t\t%s<<8;\n", varName));
+                sb.append(String.format("\t\t\t%s|=Serial.read();\n", varName));
+                sb.append(String.format("\t\t\t%s<<8;\n", varName));
+                sb.append(String.format("\t\t\t%s|=Serial.read();\n", varName));
             }
         }
-        sb.append(String.format("rob.%s",m.getName()));
+        sb.append(String.format("\t\t\trob.%s",m.getName()));
         sb.append("(");
 
         for(int i=0;i<m.getParameters().length;i++){
